8915994ed5c1db806cde90816bd6393395d5654e
toby044 <tobiasnolesen@gmail.com>
2022-02-21 15:58:07 +0100

    ok

--- login/db.php
+++ login/db.php
@@ -2,10 +2,10 @@
 $host = 'localhost';
 $dbuser = 'root';
 $dbpass = '';
-$db = 'test';
+$db = 'to_do_list';
 
 $conn = new mysqli($host, $dbuser, $dbpass, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-  }
+}
 
