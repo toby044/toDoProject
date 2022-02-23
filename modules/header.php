327f046073879d0508bacd13dd7630cc36772148
toby044 <tobiasnolesen@gmail.com>
2022-02-23 11:16:19 +0100

    Style & header changes

--- modules/header.php
+++ modules/header.php
@@ -2,18 +2,18 @@
 
 if (isset($_COOKIE['uname'])) {
     echo '<aside>
-        <div class="flex-down">
-            <h2><a href="index.php" class="header-link">To Do</a></h2>
-            <a href="all.php">Historik</a>
-        </div>
-        <a href="modules/logout.php" class="logoutBtn">Log ud</a>
-    </aside>';
+            <div class="flex-down">
+                <h2><a href="index.php" class="header-link">To Do</a></h2>
+                <a href="all.php">Historik</a>
+            </div>
+            <a href="modules/logout.php" class="logoutBtn">Log ud</a>
+        </aside>';
 } else {
     echo '<aside>
-    <div class="flex-down">
-        <h2>To Do</h2>
-    </div>
-</aside>';
+            <div class="flex-down">
+                <h2>To Do</h2>
+            </div>
+        </aside>';  
 }
 
 ?>
